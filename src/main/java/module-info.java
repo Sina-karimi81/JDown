bb02ba768570f023f38c802959f9b1e7aaef099f
sina <ksina8279@gmail.com>
2025-07-11 20:31:29 +0330

    bug: fixed the issue with mvn javafx:run not working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.github.sinakarimi.jdown {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
     requires org.kordamp.bootstrapfx.core;
     requires java.net.http;
     requires static lombok;
